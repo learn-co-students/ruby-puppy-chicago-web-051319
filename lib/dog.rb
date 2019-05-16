7743d2335396e33c368367812c4f620ec3173661
afhare <alexandra.f.hare@gmail.com>
2019-05-16 12:57:01 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Dog
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+        #binding.pry
+    end
+
+    def self.all
+        @@all.each {|dog| puts dog.name}
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+end
